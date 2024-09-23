0f23e7141cdeff6127d3da6d27681f5718337543
Vtzaom <victorrlima7@gmail.com>
2024-09-23 10:30:47 -0300

    add arquivo start/shield

--- ecommerce/start/routes.ts
+++ ecommerce/start/routes.ts
@@ -9,4 +9,13 @@
 
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home')
+const UsersController = () => import('../app/controllers/user_controllers.js')
+
+router
+  .group(() => {
+    router.get('/', [UsersController, 'index']).as('lista')
+    router.get('/:id', [UsersController, 'show']).where('id', router.matchers.number()).as('show')
+    router.post('/', [UsersController, 'create']).as('create')
+  })
+  .prefix('users')
+  .as('users')
